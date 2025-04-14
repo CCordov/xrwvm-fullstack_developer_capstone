87011e5141bd7e5e9f4af4b82006f9427286e9ec
ccordov <cristian.cordova@gmail.com>
2025-04-13 20:34:57 -0400

    Actualizacion

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,11 +30,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://cristiancord-8000.theiadockernext-1-labs-prod-theiak8s-4-' \
+    'https://cristiancord-8000.theiadockernext-1-labs-prod-theiak8s-4-'
     'tor01.proxy.cognitiveclass.ai'
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://cristiancord-8000.theiadockernext-1-labs-prod-theiak8s-4-' \
+    'https://cristiancord-8000.theiadockernext-1-labs-prod-theiak8s-4-'
     'tor01.proxy.cognitiveclass.ai'
 ]
 
@@ -101,7 +101,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.'
+        'UserAttributeSimilarityValidator',
     },
     {
         'NAME':
